4c9d85c5bbc1c4a6b9a97baa2ca16b2a43db7dcd
SOHAM GURJAR <sohamgurjar@SOHAMs-MacBook-Pro.local>
2023-05-03 22:14:49 -0500

    Day 2 - Project 5

---
+++ 5-meme-generator/src/components/Header.js
@@ -0,0 +1,11 @@
+function Header() {
+  return (
+    <header className="header">
+        <img src="../images/troll-face.png" alt="Troll face" className="header--image" />
+        <h2 className="header--title">Meme Generator</h2>
+        <h4 className="header--project">React Course - Project 3</h4>
+    </header>
+  )
+}
+
+export default Header;
---
+++ 5-meme-generator/src/components/Meme.js
@@ -0,0 +1,73 @@
+import { useState, useEffect } from "react";
+
+function Meme() {
+  // const [memeImage, setMemeImage] = useState("https://i.imgflip.com/30b1gx.jpg")
+
+  const [meme, setMeme] = useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "https://i.imgflip.com/30b1gx.jpg",
+  });
+
+  const [allMemes, setAllMemes] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.imgflip.com/get_memes")
+      .then((res) => res.json())
+      .then((data) => setAllMemes(data.data.memes));
+  }, []);
+
+  function getMemeImage() {
+    const randomNumber = Math.floor(Math.random() * allMemes.length);
+    const url = allMemes[randomNumber].url;
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      randomImage: url,
+    }));
+  }
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      [name]: value,
+    }));
+  }
+
+  return (
+    <main>
+      <div className="form">
+        <input
+          type="text"
+          placeholder="Top Text"
+          className="form--input"
+          name="topText"
+          value={meme.topText}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          placeholder="Bottom Text"
+          className="form--input"
+          name="bottomText"
+          value={meme.bottomText}
+          onChange={handleChange}
+        />
+        <button className="form--button" onClick={getMemeImage}>
+          Get a new meme image 🖼️
+        </button>
+      </div>
+      <div className="meme">
+        <img
+          src={meme.randomImage}
+          alt="memeTemplate"
+          className="meme--image"
+        />
+        <h2 className="meme--text top">{meme.topText}</h2>
+        <h2 className="meme--text bottom">{meme.bottomText}</h2>
+      </div>
+    </main>
+  );
+}
+
+export default Meme;
